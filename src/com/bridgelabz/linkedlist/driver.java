100b881431ba9750cb10a2d37faaab0eee22f70d
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-02-06 12:47:20 +0530

    Added Remove last Node

--- src/com/bridgelabz/linkedlist/driver.java
+++ src/com/bridgelabz/linkedlist/driver.java
@@ -3,9 +3,10 @@ package com.bridgelabz.linkedlist;
 public class driver {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.insert(56,0);
-        list.insert(70,56);
-        list.insert(30,56);
+        list.insert(56);
+        list.insert(70);
+        list.insert(30);
+        list.deletelastnode();
         list.display();
     }
 }
